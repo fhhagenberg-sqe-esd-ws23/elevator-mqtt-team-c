4489a3d65b1132441e5cf3e70ad4d6ba5844f4fd
Philipp Freislich <philipp.freislich@plincs.com>
2023-11-19 17:23:04 +0100

    Add ElevatorService and empty ElevatorServiceTest.

---
+++ src/test/java/at/fhhagenberg/sqelevator/service/ElevatorServiceTest.java
@@ -0,0 +1,12 @@
+package at.fhhagenberg.sqelevator.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ElevatorServiceTest {
+
+  @Test
+  void testBuildingConstructor_normalSize_BuildingIsCreated() {
+    Assertions.assertTrue(true);
+  }
+}
